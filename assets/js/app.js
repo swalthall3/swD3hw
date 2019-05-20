89eb7f6e9a726b411a6e23ca84ea4dddb15e30fc
essaydoubleyou <stephen.walthall@gmail.com>
2019-05-19 22:01:09 -0400

    first upload

---
+++ assets/js/app.js
@@ -0,0 +1,120 @@
+// @TODO: YOUR CODE HERE!
+// The code for the chart is wrapped inside a function that
+// automatically resizes the chart
+function makeResponsive() {
+
+    // if the SVG area isn't empty when the browser loads,
+    // remove it and replace it with a resized version of the chart
+    var svgArea = d3.select("body").select("svg");
+  
+    // clear svg is not empty
+    if (!svgArea.empty()) {
+      svgArea.remove();
+    }
+  
+    // SVG wrapper dimensions are determined by the current width and
+    // height of the browser window.
+    var svgWidth = window.innerWidth;
+    var svgHeight = window.innerHeight;
+  
+    var margin = {
+      top: 50,
+      bottom: 50,
+      right: 50,
+      left: 50
+    };
+  
+    var height = svgHeight - margin.top - margin.bottom;
+    var width = svgWidth - margin.left - margin.right;
+  
+    // Append SVG element
+    var svg = d3
+      .select("#scatter")
+      .append("svg")
+      .attr("height", svgHeight)
+      .attr("width", svgWidth);
+  
+    // Append group element
+    var chartGroup = svg.append("g")
+      .attr("transform", `translate(${margin.left}, ${margin.top})`);
+  
+    // Read CSV
+    d3.csv("../assets/data/data.csv")
+      .then(function(healthData) {
+        // parse data
+        healthData.forEach(function(data) {
+          data.poverty = +data.poverty;
+          data.smokes = +data.smokes;
+        });
+  
+        // create scales
+        var xLinearScale = d3.scaleLinear()
+          .domain(d3.extent(healthData, d => d.poverty))
+          .range([0, width]);
+  
+        var yLinearScale = d3.scaleLinear()
+          .domain([0, d3.max(healthData, d => d.smokes)])
+          .range([height, 0]);
+  
+        // create axes
+        var xAxis = d3.axisBottom(xLinearScale).ticks();
+        var yAxis = d3.axisLeft(yLinearScale).ticks();
+  
+        // append axes
+        chartGroup.append("g")
+          .attr("transform", `translate(0, ${height})`)
+          .call(xAxis);
+  
+        chartGroup.append("g")
+          .call(yAxis);
+  
+        // append circles
+        var circlesGroup = chartGroup.selectAll("circle")
+          .data(healthData)
+          .enter()
+          .append("circle").classed("stateCircle", true)
+          .attr("cx", d => xLinearScale(d.poverty))
+          .attr("cy", d => yLinearScale(d.smokes))
+          .attr("r", "12")
+          .append("text").classed("stateText", true)
+          .text(d => d.abbr);
+        
+        // append state abbreviations
+        // var abbrGroup = chartGroup.selectAll("text")
+        // .data(healthData)
+        // .enter()
+        // .append("text")
+        // .attr("x", d => xLinearScale(d.poverty))
+        // .attr("y", d => yLinearScale(d.smokes))
+        // .text(d => d.abbr)
+        // .classed("stateText", true);  
+
+        // Step 1: Initialize Tooltip
+        var toolTip = d3.tip()
+          .attr("class", "tooltip")
+          .offset([80, -60])
+          .html(function(d) {
+            return (`<strong>${d.state}<br>Poverty: ${d.poverty}%<br>Smoking: ${d.smokes}
+            %`);
+          });
+  
+        // Step 2: Create the tooltip in chartGroup.
+        chartGroup.call(toolTip);
+  
+        // Step 3: Create "mouseover" event listener to display tooltip
+        circlesGroup.on("mouseover", function(d) {
+          toolTip.show(d, this);
+        })
+        // Step 4: Create "mouseout" event listener to hide tooltip
+          .on("mouseout", function(d) {
+            toolTip.hide(d);
+          });
+      });
+  }
+  
+  // When the browser loads, makeResponsive() is called.
+  makeResponsive();
+  
+  // When the browser window is resized, makeResponsive() is called.
+  d3.select(window).on("resize", makeResponsive);
+  
